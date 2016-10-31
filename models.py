2e1eb53183643c8babe9c44dc1d124db532832a5
Arshin Jain <arshin.jain@outlook.com>
2016-10-31 14:42:35 -0700

    Course Model - Set Meta

--- models.py
+++ models.py
@@ -8,3 +8,6 @@ class Course(Model):
     title = CharField()
     url   = CharField(unique=True)
     created_at = DateTimeField(default = datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
