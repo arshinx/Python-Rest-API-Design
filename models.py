aba844f80d1810ae7c2a3e0b75fa0a1c85f09945
Arshin Jain <arshin.jain@outlook.com>
2016-10-31 14:42:03 -0700

    Course Model

--- models.py
+++ models.py
@@ -5,4 +5,6 @@ from peewee import *
 DATABASE = Sqlitedatabase('courses.sqlite')
 
 class Course(Model):
-    
+    title = CharField()
+    url   = CharField(unique=True)
+    created_at = DateTimeField(default = datetime.datetime.now)
