e33d57df9542c26f4b2481ab1cc8c1e31e8bdd1b
Arshin Jain <arshin.jain@outlook.com>
2016-10-31 15:43:09 -0700

    Course resource - GET

--- resources/courses.py
+++ resources/courses.py
@@ -6,3 +6,7 @@ import models
 class CourseList(Resource):
     def get(self):
         return jsonify({'courses': [{'title': 'Python Basics'}] })
+
+class Course(Resource):
+    def get(self, id):
+        return jsonify({'title': 'Python Basics'})
