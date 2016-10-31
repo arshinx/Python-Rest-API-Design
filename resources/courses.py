a18af7c226cf5cf6cc380184b441d1d1dbbb0c13
Arshin Jain <arshin.jain@outlook.com>
2016-10-31 15:41:27 -0700

    Course List Resource in Courses

--- resources/courses.py
+++ resources/courses.py
@@ -2,3 +2,7 @@
 from flask import jsonify
 from flask.ext.restfull import Resource
 import models
+
+class CourseList(Resource):
+    def get(self):
+        return jsonify({'courses': [{'title': 'Python Basics'}] })
