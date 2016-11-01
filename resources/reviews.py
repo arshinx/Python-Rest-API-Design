6df15529229b0cabce9e942f91f724951a26ef77
Arshin Jain <arshin.jain@outlook.com>
2016-11-01 09:20:16 -0700

    Construct Reviews API using Blueprint

--- resources/reviews.py
+++ resources/reviews.py
@@ -16,3 +16,9 @@ class Review(Resource):
 
     def delete(self, id):
         return jsonify({'course': 1, 'rating': 5})
+
+# Construct Api
+reviews_api = Blueprint('resources.reviews', __name__)
+api = Api(reviews_api)
+api.add_resource(ReviewList, '/reviews', endpoint='/reviews')
+api.add_resource(Review, '/reviews/<int:id>', endpoint='/review')
