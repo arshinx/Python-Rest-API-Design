6098c42102c0a667a9ec9c2e5bd43d4f8629c9b4
Arshin Jain <arshin.jain@outlook.com>
2016-11-01 09:09:15 -0700

    Register Courses API with Blueprint

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask
 
 # Local Imports
 import models
-from resources.courses import course_api
+from resources.courses import courses_api
 
 DEBUG = True
 HOST  = '0.0.0.0'
 PORT  = 8000
 
 app = Flask(__name__)
-
+app.register_blueprint(courses_api)
 
 # Routes
 
